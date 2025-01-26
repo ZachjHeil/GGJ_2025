314da556331dc6a3ac6fb46e03a75b63b71ea6a0
Loasty <tadyand147@gmail.com>
2025-01-26 14:54:43 -0500

    Main Pushes

---
+++ GGJ2025_Unity/Assets/Puzzle_FirstPuzzle.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Puzzle_FirstPuzzle : MonoBehaviour
+{
+    InputManager input;
+
+    private void Start()
+    {
+        input = InputManager.Instance;
+    }
+
+    private void Update()
+    {
+        if(input.PlayerCancel() || input.PlayerDashed())
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
