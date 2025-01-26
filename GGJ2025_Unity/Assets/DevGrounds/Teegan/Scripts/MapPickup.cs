54685bd4ae3e134d0cd8f875d61192b27de935ce
Loasty <tadyand147@gmail.com>
2025-01-26 13:46:28 -0500

    Placed objectives

--- GGJ2025_Unity/Assets/DevGrounds/Teegan/Scripts/MapPickup.cs
+++ GGJ2025_Unity/Assets/DevGrounds/Teegan/Scripts/MapPickup.cs
@@ -7,24 +7,11 @@ public class MapPickup : MonoBehaviour
 {
     public Enums.InventoryItems inventoryItem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public void TriggerItemInteract()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Inventory.Instance.ObtainItem(inventoryItem);
-            gameObject.SetActive(false);
-           
-        }
+        Inventory.Instance.ObtainItem(inventoryItem);
+        gameObject.SetActive(false);
     }
 
 
